eee73f2ce25a64a0395e5ed3755bd63b867e7bfc
Olli Suutari <olli.suutari@jkl.fi>
2018-11-26 17:37:54 +0200

    Update custom.js

--- test/keski-testi/themes/custom/js/custom.js
+++ test/keski-testi/themes/custom/js/custom.js
@@ -1,5 +1,9 @@
 /* Add your custom template javascript here */
 
 function finnaCustomInit() {
+    console.log("not this.");
+}
+
+function test() {
     console.log("Another day, an another commit!");
 }
