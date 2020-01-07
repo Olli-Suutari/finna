eea5c12753bae066de687e049dc1df19e4499d35
Olli Suutari <olli.suutari@jkl.fi>
2020-01-07 13:58:28 +0200

    Remove keski-testi

--- test/keski-testi/themes/custom/js/custom.js
+++
@@ -1,9 +0,0 @@
-/* Add your custom template javascript here */
-
-function finnaCustomInit() {
-    console.log("not this.");
-}
-
-function test() {
-    console.log("Another day, an another commit!");
-}
