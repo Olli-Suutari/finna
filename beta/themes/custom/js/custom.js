a03a3abf253aa63ceb8d85d18567d184d76b7d26
JK\suutarol <olli.suutari@jkl.fi>
2020-01-16 13:28:34 +0200

    Navigation etc.

--- beta/themes/custom/js/custom.js
+++ beta/themes/custom/js/custom.js
@@ -1,4 +1,22 @@
 /* Add your custom template javascript here */
+var locationUrl = "";
+
+
+function loadJsOrCssFile(filename, filetype){
+    if (filetype=="js"){ //if filename is a external JavaScript file
+        var fileref=document.createElement('script')
+        fileref.setAttribute("type","text/javascript")
+        fileref.setAttribute("src", filename)
+    }
+    else if (filetype=="css"){ //if filename is an external CSS file
+        var fileref=document.createElement("link")
+        fileref.setAttribute("rel", "stylesheet")
+        fileref.setAttribute("type", "text/css")
+        fileref.setAttribute("href", filename)
+    }
+    if (typeof fileref!="undefined")
+        document.getElementsByTagName("head")[0].appendChild(fileref)
+}
 
 // Function for checking if page exists and redirecting accordingly.
 function checkUrlForContent(url) {
@@ -16,10 +34,12 @@ function appendSearchBar () {
     if(window.location.href == "https://keski.finna-test.fi/beta/") {
         $('.searchbox-home').removeClass('searchbox-home');
         $('.search-links').css("display", "none");
-        $('.search').append('<a href="/beta/Search/Advanced" class="btn btn-link btn-advanced"><i class="fa fa-search-adv"></i>Tarkennettu haku</a>')
+        $('.search').append('<a href="/beta/Search/Advanced" class="btn btn-link btn-advanced"><i class="fa fa-search-adv"></i> Tarkennettu haku</a>')
+        $('.search').append('<a href="/beta/Content/Help" class="btn btn-link btn-advanced"><i class="fa fa-info-circle"></i> Hakuohjeet</a>')
     }
     else {
-        $('.search').append('<a href="/beta/Search/Advanced" class="btn btn-link btn-advanced"><i class="fa fa-search-adv"></i>Tarkennettu haku</a>')
+        $('.search').append('<a href="/beta/Search/Advanced" class="btn btn-link btn-advanced"><i class="fa fa-search-adv"></i> Tarkennettu haku</a>')
+        $('.search').append('<a href="/beta/Content/Help" class="btn btn-link btn-advanced"><i class="fa fa-info-circle"></i> Hakuohjeet</a>')
     }
 
     if (window.location.href.indexOf('/Search/History') > -1) {
@@ -41,42 +61,55 @@ function appendSearchBar () {
 }
 
 function addIconsToMainNavigation() {
-    $('a[href$="/kirjastot"]').prepend('<i class="fa fa-building"></i> ')
-    $('a[href$="/info"]').prepend('<i class="fa fa-question-circle"></i> ')
-    $('a[href$="/ekirjasto"]').prepend('<i class="fa fa-globe"></i> ')
-    $('.fa-nav-menu_Vinkit').append('<i class="fa fa-archive"></i> ')
-    $('a[href$="/Feedback/Home"]').prepend('<i class="fa fa-comment"></i> ')
-
+    $('header a[href$="/kirjastot"]').prepend('<i class="fa fa-building"></i> ')
+    $('header a[href$="/info"]').prepend('<i class="fa fa-question-circle"></i> ')
+    $('header a[href$="/ekirjasto"]').prepend('<i class="fa fa-globe"></i> ')
+    $('header .fa-nav-menu_Vinkit').append('<i class="fa fa-archive"></i> ')
+    $('header li a[href$="/Feedback/Home"]').prepend('<i class="fa fa-comment"></i> ')
     //$('a[href$="/kirjastot"]').prepend('<i class="fa fa-history"></i> ')
-}
-
-
-function loadjscssfile(filename, filetype){
-    if (filetype=="js"){ //if filename is a external JavaScript file
-        var fileref=document.createElement('script')
-        fileref.setAttribute("type","text/javascript")
-        fileref.setAttribute("src", filename)
+    if (window.location.href == 'https://keski.finna-test.fi/beta/') {
+        $('.navbar-brand').addClass("selected-nav");
     }
-    else if (filetype=="css"){ //if filename is an external CSS file
-        var fileref=document.createElement("link")
-        fileref.setAttribute("rel", "stylesheet")
-        fileref.setAttribute("type", "text/css")
-        fileref.setAttribute("href", filename)
+    if (window.location.href.indexOf('/Content/kirjastot') > -1) {
+        $('header li a[href$="/kirjastot"]').addClass("selected-nav");
     }
-    if (typeof fileref!="undefined")
-        document.getElementsByTagName("head")[0].appendChild(fileref)
-}
+    else if (window.location.href.indexOf('/Content/info') > -1) {
+        $('header li a[href$="/info"]').addClass("selected-nav");
+    }
+    else if (window.location.href.indexOf('/Content/ekirjasto') > -1) {
+        $('header li a[href$="/ekirjasto"]').addClass("selected-nav");
+    }
+    else if (window.location.href.indexOf('/Feedback/Home') > -1) {
+        $('header li a[href$="/Feedback/Home"]').addClass("selected-nav");
+    }
+    else {
+        var linksInTipsMenu = [];
+        $('#menu_Vinkit a').each(function(){
+            //do something with the link element
+            //console.log(this.href)
+            if (window.location.href.indexOf(this.href) > -1) {
+                $('#menu_Vinkit').addClass("selected-nav");
+                $('#menu_Vinkit').addClass("selected-nav");
+                var linkEnding = this.href.substring(this.href.lastIndexOf("/") + 1);
+                console.log(linkEnding)
 
+                $('li a[href$="' + linkEnding + '"]').addClass("selected-nav");
+
+            }
+         });
+    }
+}
 
 function finnaCustomInit() {
-    loadjscssfile("https://fonts.googleapis.com/css?family=Lato|Open+Sans&display=swap", "css") 
 
-    //loadjscssfile("https://use.fontawesome.com/releases/v5.12.0/css/all.css", "css") 
-    //loadjscssfile("https://use.fontawesome.com/releases/v5.12.0/css/v4-shims.css", "css")
-    //loadjscssfile("https://use.fontawesome.com/releases/v5.12.0/js/all.js", "js")
-    //loadjscssfile("https://use.fontawesome.com/releases/v5.12.0/js/v4-shims.js", "js")
+    loadJsOrCssFile("https://fonts.googleapis.com/css?family=Lato|Open+Sans&display=swap", "css") 
 
-    var locationUrl = window.location.href;
+    //loadJsOrCssFile("https://use.fontawesome.com/releases/v5.12.0/css/all.css", "css") 
+    //loadJsOrCssFile("https://use.fontawesome.com/releases/v5.12.0/css/v4-shims.css", "css")
+    //loadJsOrCssFile("https://use.fontawesome.com/releases/v5.12.0/js/all.js", "js")
+    //loadJsOrCssFile("https://use.fontawesome.com/releases/v5.12.0/js/v4-shims.js", "js")
+
+    locationUrl = window.location.href;
     // Re-direct to /Content/Foo if using lowercase /content/
     if (locationUrl.indexOf('/content/') > -1) {
         window.location = locationUrl.replace('/content/', '/Content/');
