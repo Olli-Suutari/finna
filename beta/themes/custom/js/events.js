9f787a2a5b9d8884d73127bd2d8908f04ad39fb4
Olli Suutari <olli.suutari@jkl.fi>
2020-03-11 15:32:04 +0200

    Update events.js

--- beta/themes/custom/js/events.js
+++ beta/themes/custom/js/events.js
@@ -1,19 +1,46 @@
-$.ajax("https://keski-finna.fi/wp-json/acf/v3/events", {
-    accepts:{
-        xml:"application/json"
-    },
-    dataType:"json",
-    success:function(data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            var event = data[i].acf;
-            console.log(event);
-        }
-    },
-    error: function (request, status, error) {
-        console.log(error)
-    },
-    complete: function () {
+var isEventsPage = $('.events-page').length === 1;
 
+function  fetchEvents() {
+    $.ajax("https://keski-finna.fi/wp-json/acf/v3/events", {
+        accepts:{
+            xml:"application/json"
+        },
+        dataType:"json",
+        success:function(data) {
+            console.log(data);
+            generateEventList(data);
+        },
+        error: function (request, status, error) {
+            console.log(error)
+        },
+        complete: function () {
+
+        }
+    });
+}
+
+function generateEventList(events) {
+    for (var i = 0; i < events.length; i++) {
+        var event = events[i].acf;
+        console.log(event);
+        // Always set finnish as a fallback.
+        var itemTitleFi = "";
+        var itemTitleEn = null;
+        var itemContentFi = "";
+        var itemContentEn = null;
+        var itemStartDate = "";
+        var prettyDate = "";
+        var itemLink = null;
+        var itemImg = null;
+        var itemUrl = "";
+        var organizers = [];
+        var customAddress = null;
+        var tags = [];
+        var price = 0;
     }
-});
+}
+
+if (isEventsPage) {
+    fetchEvents();
+}
+
