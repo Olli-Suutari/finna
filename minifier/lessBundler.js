dc492092e4d5f2ca2ec4b6ae1192c2a9d211c506
Olli-Suutari <suutari.olli+github@gmail.com>
2021-02-16 00:25:26 +0200

    Less bundler wip

---
+++ minifier/lessBundler.js
@@ -0,0 +1,31 @@
+/*
+* This file is used for generating a bundle of less files.
+* based on: https://stackoverflow.com/questions/24205110/combining-two-less-files-in-one
+*/
+//import {mergeFiles} from 'merge-files';
+
+mergeFiles = require('merge-files');
+
+const fs = require( 'fs' );
+
+
+ 
+var path = '../external/style/'
+
+
+const outputPath = __dirname + '/result.less';
+ 
+const inputPathList = [
+  path + './newCustom.less',
+  path + './newNavigation.less',
+  path + './newsAndEvents.less',
+  path + './newSearch.less'
+];
+ 
+// status: true or false
+//const status = await mergeFiles(inputPathList, outputPath);
+// or
+mergeFiles(inputPathList, outputPath).then((status) => {
+    // next
+    console.log("next")
+});
