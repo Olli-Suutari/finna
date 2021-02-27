d58bf8dfa4cc2ce4d8c966e40e00f5fd2d9f7dc7
Olli-Suutari <suutari.olli+github@gmail.com>
2021-02-27 14:19:50 +0200

    Cleanup on style bundler + moved bundle to dist folder

--- minifier/lessBundler.js
+++ minifier/lessBundler.js
@@ -1,27 +1,18 @@
 /*
 * This file is used for generating a bundle of less files.
-* based on: https://stackoverflow.com/questions/24205110/combining-two-less-files-in-one
+* Bundle is placed on external/js/dist since it's easy to ignore and other dist files are there.
 */
-//import {mergeFiles} from 'merge-files';
-
 mergeFiles = require('merge-files');
 
-const fs = require( 'fs' );
-
 const path = '../external/style/'
-const outputPath = '../external/style/bundle.less';
- 
+const outputPath = '../external/js/dist/bundle.less';
 const inputPathList = [
   path + './newCustom.less',
   path + './newNavigation.less',
   path + './newsAndEvents.less',
   path + './newSearch.less'
 ];
- 
-// status: true or false
-//const status = await mergeFiles(inputPathList, outputPath);
-// or
+
 mergeFiles(inputPathList, outputPath).then((status) => {
-    // next
-    console.log("next")
+    console.log("Bundle complete!")
 });
