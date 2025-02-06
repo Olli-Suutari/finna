2a9f382f35912e82008410cb5658a32fa0ba0650
Olli Suutari <35490459+Olli-Suutari@users.noreply.github.com>
2025-02-06 20:56:51 +0200

    Added missing hold & theme.config files

---
+++ keski/themes/custom/theme.config.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'extends' => 'finna2',
+    'css' => [
+        'custom.css',
+        'settings.css'
+    ],
+    'js' => [
+        'custom.js',
+        'https://keski-finna.fi/external/finna/js/dist/main.js',
+    ]
+];
